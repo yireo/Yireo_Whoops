a73908c95d6d550d4f408229915717082e4312c1
Jisse Reitsma <jisse@yireo.com>
2019-10-14 16:53:38 +0200

    New configuration class

---
+++ Config/Config.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Yireo\Whoops\Config;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ * Class Config
+ *
+ * @package Yireo\Whoops\Config
+ */
+class Config
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Config constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEditor(): string
+    {
+        return (string)$this->scopeConfig->getValue('yireo_whoops/settings/editor');
+    }
+}
